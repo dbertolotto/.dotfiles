2500176a00e7da92935439caf9ff4a0349add692
Davide Bertolotto <davide.bertolotto@gmail.com>
2019-11-26 17:46:48 +0000

    added clojure and fzf

--- debian-extras.sh
+++ debian-extras.sh
@@ -4,10 +4,12 @@ apti='apt install -y --no-install-recommends'
 apt update -y && apt upgrade -y
 
 # utility packages
-$apti bash-completion
+$apti bash-completion fzf
 # necessary for youcompleteme (vim plugin)
 $apti build-essential cmake python3-dev
-# jdk
+# java
 $apti default-jdk default-jre
+# clojure
+$apti clojure leiningen
 # vim & emacs
 $apti vim vim-python-jedi emacs
