1326b3d53d97e8bdad06c0a4894aa31f807ff61e
Davide Bertolotto <davide.bertolotto@gmail.com>
2020-03-10 22:14:57 +0100

    mame build

--- emulationstation.sh
+++ emulationstation.sh
@@ -9,9 +9,9 @@ apt update -y && apt upgrade -y
 $apti libsdl2-dev libboost-system-dev libboost-filesystem-dev libboost-date-time-dev libboost-locale-dev libfreeimage-dev libfreetype6-dev libeigen3-dev libcurl4-openssl-dev libasound2-dev libgl1-mesa-dev build-essential cmake git
 # clone repo
 git clone https://github.com/Aloshi/EmulationStation $build_dir
-# build
 cd $build_dir
 git checkout $version
+# build
 cmake .
 make
 make install
---
+++ mame.sh
@@ -0,0 +1,18 @@
+# install emulation station
+apti='apt install -y --no-install-recommends'
+build_dir='~/.dotfiles/tmp/mame'
+version='mame0219'
+multi=5
+
+apt update -y && apt upgrade -y
+
+# dependencies
+$apti git build-essential python libsdl2-dev libsdl2-ttf-dev libfontconfig-dev qt5-default
+# clone repo
+git clone https://github.com/mamedev/mame.git $build_dir
+cd $build_dir
+git checkout $version
+# build
+make TOOLS=1 REGENIE=1 -j$multi
+cd ~/.dotfiles
+
