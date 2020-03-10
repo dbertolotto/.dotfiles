97009d26dcfaf48b5f1b2b72ed84b1c82f174bbc
Davide Bertolotto <davide.bertolotto@gmail.com>
2020-03-10 21:49:47 +0100

    emulationstation build file

---
+++ emulationstation.sh
@@ -0,0 +1,19 @@
+# install emulation station
+apti='apt install -y --no-install-recommends'
+build_dir='~/.dotfiles/tmp/EmulationStation'
+version='master'
+
+apt update -y && apt upgrade -y
+
+# dependencies
+$apti libsdl2-dev libboost-system-dev libboost-filesystem-dev libboost-date-time-dev libboost-locale-dev libfreeimage-dev libfreetype6-dev libeigen3-dev libcurl4-openssl-dev libasound2-dev libgl1-mesa-dev build-essential cmake git
+# clone repo
+git clone https://github.com/Aloshi/EmulationStation $build_dir
+# build
+cd $build_dir
+git checkout $version
+cmake .
+make
+make install
+cd ~/.dotfiles
+
