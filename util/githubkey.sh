8db80621876a7b9ccc6a12c1a20c802a6ccf3711
Davide Bertolotto <davide.bertolotto@netcetera.com>
2021-11-11 11:39:00 +0100

    fixed github key generation

--- util/githubkey.sh
+++ util/githubkey.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 
 # key file location
-keypath=~/.ssh/id_rsa
+keyfile=~/.ssh/id_github
 
 # generate key
-ssh-keygen -t rsa -b 4096 -C "davide.bertolotto@gmail.com" $keypath
+ssh-keygen -t rsa -b 4096 -C "davide.bertolotto@gmail.com" -f $keyfile
 # start the agent and add the key
 eval "$(ssh-agent -s)"
-ssh-add $keypath
+ssh-add $keyfile
 # show public key
-cat $keypath.pub
+cat $keyfile.pub
